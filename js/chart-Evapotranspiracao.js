f5035d9af3c908ba6c39479825ebfbe8da27eaa6
Nathan Silva <114961313+Nathansilva20@users.noreply.github.com>
2024-06-22 08:13:30 -0300

    mudança no calculo de evapotranspiração

--- js/chart-Evapotranspiracao.js
+++ js/chart-Evapotranspiracao.js
@@ -1,6 +1,11 @@
-let myLineChart5; // Variável global para armazenar a instância do quarto gráfico
+let myLineChart5; // Variável global para armazenar a instância do gráfico
 
-// Função para renderizar o quarto gráfico
+// Objeto para armazenar a precipitação selecionada
+const precipitacao = {
+    valor: null
+};
+
+// Função para renderizar o gráfico inicialmente
 function renderEvapotranspiracaoChart(labels, data) {
     const ctx = document.getElementById("EvapoChart");
     if (ctx) {
@@ -39,7 +44,7 @@ function renderEvapotranspiracaoChart(labels, data) {
                     yAxes: [{
                         ticks: {
                             min: 0,
-                            max: 100,
+                            max: 200,
                             maxTicksLimit: 5
                         },
                         gridLines: {
@@ -57,48 +62,57 @@ function renderEvapotranspiracaoChart(labels, data) {
     }
 }
 
-// Função para buscar os dados do quarto gráfico da URL JSON
-async function fetchEvapotranspiracaoData(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Erro ao buscar os dados de temperatura média.');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Erro:', error.message);
-        return null;
-    }
-}
-
-// Função para atualizar os dados do quarto gráfico
+// Função para atualizar os dados do gráfico
 async function updateEvapotranspiracaoChart() {
-    const url = 'https://fertirrigacao-0324-default-rtdb.firebaseio.com/Fazenda/temperatura.json?'; // Substitua pela sua URL real
-    const quintoGraficoData = await fetchEvapotranspiracaoData(url);
-    if (quintoGraficoData !== null) {
-        const currentTime = new Date();
-        const lastTime = currentTime.toLocaleTimeString();
+    const currentTime = new Date();
+    const lastTime = currentTime.toLocaleTimeString();
 
-        if (myLineChart5) {
-            myLineChart5.data.labels.push(lastTime);
-            myLineChart5.data.datasets[0].data.push(quintoGraficoData);
-            // Limitar o número de pontos no gráfico a 10
-            if (myLineChart5.data.labels.length > 10) {
-                myLineChart5.data.labels.shift();
-                myLineChart5.data.datasets[0].data.shift();
+    if (myLineChart5) {
+        try {
+            const temperaturaMedia = await buscarTemperaturaMedia(temperaturaMediaURL);
+
+            if (temperaturaMedia !== null) {
+                const evapotranspiracao = calcularEvapotranspiracaoBlaneyCriddle(temperaturaMedia, precipitacao.valor);
+
+                // Atualiza os dados no gráfico
+                myLineChart5.data.labels.push(lastTime);
+                myLineChart5.data.datasets[0].data.push(evapotranspiracao);
+
+                // Limita o número de pontos no gráfico a 10
+                if (myLineChart5.data.labels.length > 10) {
+                    myLineChart5.data.labels.shift();
+                    myLineChart5.data.datasets[0].data.shift();
+                }
+
+                myLineChart5.update();
+            } else {
+                console.error('Não foi possível obter os dados de temperatura média.');
             }
-            myLineChart5.update();
-            document.getElementById('ultimo-valor-evapo').innerText = 'Último valor da evapotranspiracao: ' + quintoGraficoData;
-        } else {
-            console.error('myLineChart5 não está inicializado.');
+        } catch (error) {
+            console.error('Erro ao atualizar o gráfico:', error);
         }
     } else {
-        console.error('Não foi possível obter os dados do quarto gráfico.');
+        console.error('myLineChart5 não está inicializado.');
     }
 }
 
-// Função para buscar os dados de temperatura média da URL JSON
+// Chamar a função para renderizar o gráfico e atualizar a evapotranspiração ao carregar a página
+window.addEventListener('load', function () {
+    const labels5 = [];
+    const data5 = [];
+    renderEvapotranspiracaoChart(labels5, data5);
+    setInterval(updateEvapotranspiracaoChart, 3000);
+
+    // Verifica se há uma região previamente selecionada e atualiza o botão do dropdown
+    const regiaoSelecionada = localStorage.getItem('regiaoSelecionada');
+    if (regiaoSelecionada) {
+        aplicarRegra(regiaoSelecionada);
+    }
+});
+
+const temperaturaMediaURL = 'https://fertirrigacao-0324-default-rtdb.firebaseio.com/Fazenda/temperatura.json?';
+
+// Função para buscar os dados de temperatura média
 async function buscarTemperaturaMedia(url) {
     try {
         const response = await fetch(url);
@@ -106,39 +120,37 @@ async function buscarTemperaturaMedia(url) {
             throw new Error('Erro ao buscar os dados de temperatura média.');
         }
         const data = await response.json();
-        return data;
+        return data; // Supondo que os dados já sejam um valor numérico
     } catch (error) {
         console.error('Erro:', error.message);
         return null;
     }
 }
 
-// Função para calcular a evapotranspiração usando o método Blaney-Criddle
+// Função para calcular a evapotranspiração utilizando o método de Blaney-Criddle
 function calcularEvapotranspiracaoBlaneyCriddle(temperaturaMedia, precipitacao) {
-    // Fórmula Método Blaney-Criddle
-    return ((temperaturaMedia * 0.46) + 8.13) * precipitacao;
+    // Fórmula para o método de Blaney-Criddle
+    const evapotranspiracao = ((temperaturaMedia * 0.46) + 8.13) * precipitacao;
+    return evapotranspiracao;
 }
 
-// Função para atualizar a evapotranspiração
-async function updateEvapotranspiracao() {
-    const temperaturaMediaURL = 'https://fertirrigacao-0324-default-rtdb.firebaseio.com/Fazenda/temperatura.json?';
-    const precipitacao = 7.8;
+// Função para aplicar a regra ao selecionar uma região
+function aplicarRegra(regiao) {
+    const regioes = {
+        "Região Metropolitana": 10,
+        "Litoral Sul": 10.5,
+        "Norte": 11,
+        "Serrana": 11,
+        "Central": 11
+    };
 
-    const temperaturaMedia = await buscarTemperaturaMedia(temperaturaMediaURL);
-    if (temperaturaMedia !== null) {
-        const evapotranspiracao = calcularEvapotranspiracaoBlaneyCriddle(temperaturaMedia, precipitacao);
-        const evp = document.getElementById("ultimo-valor-evapo");
-        evp.textContent = 'Evapotranspiração atual: ' + evapotranspiracao.toFixed(2); // Arredondar para 2 casas decimais
+    if (regiao in regioes) {
+        precipitacao.valor = regioes[regiao];
+        document.getElementById('dropdownRegioes').textContent = `Região Selecionada: ${regiao}`;
+
+        // Armazena a região selecionada no localStorage
+        localStorage.setItem('regiaoSelecionada', regiao);
     } else {
-        console.error('Não foi possível obter os dados de temperatura média.');
+        console.error(`Região '${regiao}' não encontrada.`);
     }
 }
-
-// Chamar a função para renderizar o quarto gráfico e atualizar a evapotranspiração ao carregar a página
-window.addEventListener('load', function () {
-    const labels5 = [];
-    const data5 = [];
-    renderEvapotranspiracaoChart(labels5, data5);
-    setInterval(updateEvapotranspiracaoChart, 3000);
-    updateEvapotranspiracao();
-});
